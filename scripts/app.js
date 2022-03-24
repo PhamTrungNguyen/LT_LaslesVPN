ed93f38ee28be4332d175ca9782dd66f2dedb90a
PhamTrungNguyen <trungnguyen010102hl@gmail.com>
2022-03-24 18:13:14 +0700

    no message

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,6 @@ $(".customer-list").slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 1 /* scroll 1 cai */,
-  dots: true, /* cục tròn */
   /* css slick arrow */
   prevArrow:`<button type='button' class='slick-prev pull-left'><img src="./images/arrow-left.svg" /></button>`, 
   nextArrow:`<button type='button' class='slick-next pull-right'><img src="./images/arrow-right.svg" /></button>`,
